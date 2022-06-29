b3739e89e1482b7422fa406db9518b9d969b22bc
lucasbennesby <lucasbenesby@gmail.com>
2022-06-29 17:48:13 -0300

    questao 3

---
+++ 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne.js
@@ -0,0 +1,171 @@
+let faturamento = [
+  {
+    dia: 1,
+    valor: 22174.1664,
+  },
+  { dia: 2, valor: 24537.6698 },
+  {
+    dia: 3,
+    valor: 26139.6134,
+  },
+  {
+    dia: 4,
+    valor: 0.0,
+  },
+  {
+    dia: 5,
+    valor: 0.0,
+  },
+  {
+    dia: 6,
+    valor: 26742.6612,
+  },
+  {
+    dia: 7,
+    valor: 0.0,
+  },
+  {
+    dia: 8,
+    valor: 42889.2258,
+  },
+  {
+    dia: 9,
+    valor: 46251.174,
+  },
+  {
+    dia: 10,
+    valor: 11191.4722,
+  },
+  {
+    dia: 11,
+    valor: 0.0,
+  },
+  {
+    dia: 12,
+    valor: 0.0,
+  },
+  {
+    dia: 13,
+    valor: 3847.4823,
+  },
+  {
+    dia: 14,
+    valor: 373.7838,
+  },
+  {
+    dia: 15,
+    valor: 2659.7563,
+  },
+  {
+    dia: 16,
+    valor: 48924.2448,
+  },
+  {
+    dia: 17,
+    valor: 18419.2614,
+  },
+  {
+    dia: 18,
+    valor: 0.0,
+  },
+  {
+    dia: 19,
+    valor: 0.0,
+  },
+  {
+    dia: 20,
+    valor: 35240.1826,
+  },
+  {
+    dia: 21,
+    valor: 43829.1667,
+  },
+  {
+    dia: 22,
+    valor: 18235.6852,
+  },
+  {
+    dia: 23,
+    valor: 4355.0662,
+  },
+  {
+    dia: 24,
+    valor: 13327.1025,
+  },
+  {
+    dia: 25,
+    valor: 0.0,
+  },
+  {
+    dia: 26,
+    valor: 0.0,
+  },
+  {
+    dia: 27,
+    valor: 25681.8318,
+  },
+  {
+    dia: 28,
+    valor: 1718.1221,
+  },
+  {
+    dia: 29,
+    valor: 13220.495,
+  },
+  {
+    dia: 30,
+    valor: 8414.61,
+  },
+];
+
+function menorFaturamento() {
+  var menorValor = 99999999;
+  for (i = 0; i < faturamento.length; i++) {
+    if (faturamento[i].valor != 0 && faturamento[i].valor < menorValor) {
+      menorValor = faturamento[i].valor;
+    }
+  }
+  console.log(
+    `O menor valor de faturamento ocorrido em um dia do mês foi ${menorValor}`
+  );
+}
+function maiorFaturamento() {
+  var maiorValor = 0;
+  for (i = 0; i < faturamento.length; i++) {
+    if (faturamento[i].valor > maiorValor) {
+      maiorValor = faturamento[i].valor;
+    }
+  }
+  console.log(
+    `O maior valor de faturamento ocorrido em um dia do mês foi ${maiorValor}`
+  );
+}
+function diasMaiorQueMedia(x) {
+  var dias = 0;
+  for (i = 0; i < faturamento.length; i++) {
+    if (faturamento[i].valor > x) {
+      dias++;
+    }
+  }
+  console.log(
+    `O número de dias no mês em que o valor de faturamento diário foi superior à média mensal é de ${dias}`
+  );
+}
+function mediaMensal() {
+  var total = 0;
+  var valorMedio = 0;
+  var a = 0;
+  for (i = 0; i < faturamento.length; i++) {
+    if (faturamento[i].valor != 0) {
+      total += faturamento[i].valor;
+      a++;
+    }
+  }
+  valorMedio = total / a;
+  //console.log(`a media de valor mensal foi ${valorMedio}`);
+  diasMaiorQueMedia(valorMedio);
+}
+
+menorFaturamento();
+maiorFaturamento();
+mediaMensal();
